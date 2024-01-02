6b44975a340fe43e38fba393778be1c25f9167d7
Tej Pandit <67128950+tejxpandit@users.noreply.github.com>
2024-01-01 22:49:22 -0600

    Pickling of Knowledge Base
    Added a commented pickle based saving and reloading of the generated knowledge base. This might be needed when the base grows or learns on-the-fly. However, in the current pretrained state, it has been commented out as there are no dynamically added parameters.

--- dataGenerator.py
+++ dataGenerator.py
@@ -91,3 +91,79 @@ class DataStorage:
         return self.data
 
         
+#-----------------------------------------------------------------
+# GENERATOR 
+
+# import pickle
+
+# # Target File
+# file = open("commands.pkl", 'wb')
+
+# # File Generator
+# pickle.dump(actions, file)
+# pickle.dump(actions_key, file)
+# pickle.dump(targets, file)
+# pickle.dump(targets_key, file)
+# pickle.dump(descriptors, file)
+# pickle.dump(descriptors_key, file)
+# pickle.dump(agent_metanames, file)
+# pickle.dump(agent_names, file)
+
+# # Close File
+# file.close()
+
+# # Test
+# file = open('commands.pkl', 'rb')
+# act = pickle.load(file)
+# act_k = pickle.load(file)
+# tar = pickle.load(file)
+# tar_k = pickle.load(file)
+# des = pickle.load(file)
+# des_k = pickle.load(file)
+# agm = pickle.load(file)
+# agn = pickle.load(file)
+# print(act)
+# print(act_k)
+# print(tar)
+# print(tar_k)
+# print(des)
+# print(des_k)
+# print(agm)
+# print(agn)
+
+# # Verify
+# if len(act) == len(act_k):
+#     print("Actions Balanced")
+# else:
+#     print ("Actions Imbalanced")
+
+# if len(tar) == len(tar_k):
+#     print("Targets Balanced")
+# else:
+#     print ("Targets Imbalanced")
+
+# if len(des) == len(des_k):
+#     print("Descriptors Balanced")
+# else:
+#     print ("Descriptors Imbalanced")
+
+# if len(agm) == len(agn):
+#     print("Agent Names Balanced")
+# else:
+#     print ("Agent Names Imbalanced")
+
+# # Close File
+# file.close()
+
+# # Import File Function
+# def importCommandList(self, commandFile="commands.pkl"):
+#         file = open(commandFile, 'rb')
+#         self.actions = pickle.load(file)
+#         self.actions_key = pickle.load(file)
+#         self.targets = pickle.load(file)
+#         self.targets_key = pickle.load(file)
+#         self.descriptors = pickle.load(file)
+#         self.descriptors_key = pickle.load(file)
+#         self.agent_metanames = pickle.load(file)
+#         self.agent_names = pickle.load(file)
+#         file.close()
